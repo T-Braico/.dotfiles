f928c27d35176b66faa4e2a9b6504e2459110f90
Trevor Braico <tbraico.github@gmail.com>
2025-02-13 09:09:18 -0600

    created nvim command to make background transparent

--- nvim/after/plugin/colorscheme.lua
+++ nvim/after/plugin/colorscheme.lua
@@ -1,7 +1,12 @@
 require("tokyonight").setup({ style = "night" })
 vim.cmd.colorscheme('tokyonight')
 
--- Set transparent background
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+local function transparent()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
+vim.api.nvim_create_user_command("Transparent", transparent, {
+    nargs = 0,
+    desc = "Make the background transparent for all Neovim windows",
+})
