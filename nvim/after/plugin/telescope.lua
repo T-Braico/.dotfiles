ca8beb0e4643f6310b0d1da9e90151094abeef34
Trevor Braico <tbraico.github@gmail.com>
2025-02-17 07:28:47 -0600

    updated tmux config

--- nvim/after/plugin/telescope.lua
+++
@@ -1,8 +0,0 @@
-local builtins = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>pf", builtins.find_files, {})
-vim.keymap.set("n", "<leader>gf", builtins.git_files, {})
-vim.keymap.set("n", "<leader>ps", builtins.live_grep, {})
-vim.keymap.set("n", "<leader>cm", builtins.commands, {})
-vim.keymap.set("n", "<leader>co", builtins.colorscheme, {})
-vim.keymap.set("n", "<leader>sh", builtins.keymaps, {})
